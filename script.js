cda9d20d6ce0cfe666dcc9857e8f5adcb6a22e87
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-05-15 07:21:32 +0100

    Adding the some method

--- script.js
+++ script.js
@@ -61,9 +61,11 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
@@ -187,6 +189,12 @@ btnClose.addEventListener('click', function (e) {
     containerApp.style.opacity = 0;
   }
 });
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -283,3 +291,8 @@ movements.sort((a, b) => {
   }
 });
 console.log(movements);
+
+const y = Array.from({ length: 100 }, () =>
+  Math.trunc(Math.random() * 100 + 1)
+);
+console.log(y);
